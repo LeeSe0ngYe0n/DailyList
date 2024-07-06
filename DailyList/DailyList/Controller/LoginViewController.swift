09cd504c5c0dbefa27e93ce9dd362c63509b096e
LeeSeongYeon <leeseongyeon@LeeSeongYeon-MacBookPro.local>
2024-07-06 10:12:14 +0900

    feat: LoginViewController에서 버튼 AddTarget 기능 구현

--- DailyList/DailyList/Controller/LoginViewController.swift
+++ DailyList/DailyList/Controller/LoginViewController.swift
@@ -11,3 +11,24 @@ final class LoginViewController: UIViewController {
     super.viewDidLoad()
   }
 }
+
+// MARK: - Add Target
+extension LoginViewController {
+  private func setButtonAddTarget() {
+    loginView.setForgotPasswordButtonTarget(target: self, action: #selector(tappedForgotPasswordButton), for: .touchUpInside)
+    loginView.setNewCustomerButton(target: self, action: #selector(tappedForgotPasswordButton), for: .touchUpInside)
+    loginView.setLoginButtonTarget(target: self, action: #selector(tappedLoginButton), for: .touchUpInside)
+  }
+  
+  @objc private func tappedForgotPasswordButton() {
+    //
+  }
+  
+  @objc private func tappedNewCustomerButton() {
+    //
+  }
+  
+  @objc private func tappedLoginButton() {
+    //
+  }
+}
