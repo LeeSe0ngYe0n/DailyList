ae0316cbdcff19b9eb0c8be72475f2aabb235e62
LeeSeongYeon <leeseongyeon@LeeSeongYeon-MacBookPro.local>
2024-07-21 14:50:03 +0900

    feat: CalendarViewDelegate 채택 후 didTapDate 함수 구현

--- DailyList/DailyList/Controller/CalendarViewController.swift
+++ DailyList/DailyList/Controller/CalendarViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 final class CalendarViewController: UIViewController {
   private let calendarView: CalendarView = CalendarView()
+  weak var delegate: CalendarViewControllerDelegate?
   
   override func loadView() {
     self.view = calendarView
@@ -9,5 +10,14 @@ final class CalendarViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    calendarView.delegate = self
   }
+}
+
+// MARK: - Function
+extension CalendarViewController: CalendarViewDelegate {
+    func didTapDate(_ date: Date) {
+        delegate?.didSelectDate(date)
+        dismiss(animated: true, completion: nil)
+    }
 }
